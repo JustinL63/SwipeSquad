ce98620b1b94b132f24203f246be879c426da6ac
Justin Lamb <lambjg63@gmail.com>
2019-01-23 11:59:03 -0500

    superawesome

--- create-react-express/client/src/pages/home.js
+++ create-react-express/client/src/pages/home.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Proflie from "..components/Proflie";
 
 
 function Home () {
     return(
+        <div>
+            <Profile>
+            <h2> This is where your Profile will appear</h2>
+
+            </Profile>
+
+        <h1>Welcome to Swipe Squad!</h1>
+
+
+        </div>
 
     )
 }
